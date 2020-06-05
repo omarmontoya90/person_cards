e2c05d2026b0ac773c3aa3b6d78842f7c15f1e77
omar montoya <montoyaomar01@hotmail.com>
2020-06-04 21:29:07 -0500

    intro to Higher Order Components (HOC)

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './Person.css';
+import Aux from '../../../hoc/Aux'
 
 const person = (props) => {
   return (
-    <div className="Person">
+    <Aux>
       <p onClick={props.changeName}> I'm {props.name} and I am {props.age} years old!</p>
       { (props.click) && <button onClick={props.click}>Delete User</button> }
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name}/>
-    </div>
+    </Aux>
   )
 };
 
