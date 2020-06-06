53f7d6efa8ccfa37472b6c2d5f73ca284f40632e
omar montoya <montoyaomar01@hotmail.com>
2020-06-06 15:42:32 -0500

    setting state correctly when it depends on previous state

--- src/containers/App.js
+++ src/containers/App.js
@@ -13,7 +13,8 @@ class App extends Component {
       { id: "mon1", name:"Montoya", age:"23"}
     ],
     otherSatet: "some value",
-    showPersons: false
+    showPersons: false,
+    changeCounter: 0
   }
 
   nameChangedHandler = (event, id) => {
@@ -30,7 +31,12 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState( { persons: persons } );
+    this.setState((prevState, props) => {
+      return {
+        persons: persons,
+        changeCounter: prevState.changeCounter + 1
+      }
+    });
   }
 
   deletePersonHandler = (personIndex) => {
